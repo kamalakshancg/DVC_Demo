c0f9640b0431a378ccecd352f6ab4ef1cc7316f4
kamalakshancg <kamalaksha.ncg@gmail.com>
2021-09-26 12:06:03 +0530

    firt commit

---
+++ file2.py
@@ -0,0 +1,4 @@
+with open("artifact1.txt","r") as f:
+    text = f.read()
+    print(text)
+print("Extra lines are added for file2.py")
---
+++ file3.py
@@ -0,0 +1,8 @@
+with open("artifact1.txt","r") as f:
+    value = f.readlines()
+
+print(value)
+
+with open("artifact3.txt","w") as f1:
+    f1.write("Tumkur")
+
